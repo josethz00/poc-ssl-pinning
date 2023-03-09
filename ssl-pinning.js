62651097982df78aede2fa14bfc3b99ccd59c986
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-03-08 23:31:15 -0300

    refactor: change certficate file names to match the example in README

--- ssl-pinning.js
+++ ssl-pinning.js
@@ -9,9 +9,9 @@ const pins = ['7b3983a1f3e2710c11740e2aafd416d87dc24fb0c3e3e3ecf7c740a6168f6c06'
 const app = express();
 
 const options = {
-  key: fs.readFileSync('./server.key'),
-  cert: fs.readFileSync('./server.crt'),
-  ca: fs.readFileSync('./ca_bundle.crt'),
+  key: fs.readFileSync('./ca_private_key.pem'),
+  cert: fs.readFileSync('./ca_cert.pem'),
+  ca: fs.readFileSync('./ca_bundle.pem'),
   // Set the minimum TLS version to 1.2
   secureProtocol: 'TLSv1.2',
   // Verify the server's certificate against the CA bundle
