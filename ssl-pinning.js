6984f95cbb756d04deb77e7791f1d06f8f1a5b0c
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-03-08 23:13:29 -0300

    feat: get ssl pinning certificate

--- ssl-pinning.js
+++ ssl-pinning.js
@@ -9,9 +9,9 @@ const pins = ['7b3983a1f3e2710c11740e2aafd416d87dc24fb0c3e3e3ecf7c740a6168f6c06'
 const app = express();
 
 const options = {
-  key: fs.readFileSync('path/to/private/key'),
-  cert: fs.readFileSync('path/to/certificate'),
-  ca: fs.readFileSync('path/to/ca-bundle'),
+  key: fs.readFileSync('./server.key'),
+  cert: fs.readFileSync('./server.crt'),
+  ca: fs.readFileSync('./ca_bundle.crt'),
   // Set the minimum TLS version to 1.2
   secureProtocol: 'TLSv1.2',
   // Verify the server's certificate against the CA bundle
@@ -27,12 +27,15 @@ const options = {
   },
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/hello', (req, res) => {
+  return res.send('Hello World!');
 });
 
-const server = https.createServer(options, app);
+const server = https.createServer({
+  ...options,
+  secureProtocol: 'TLSv1_2_method',
+}, app);
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+server.listen(3223, () => {
+  console.log('Server running on port 3223');
 });
